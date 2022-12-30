a59c84c3834c873be1e1b77f854869f71ee90aca
aya <67550664+ayaDjellato@users.noreply.github.com>
2022-12-31 00:29:40 +0100

    Update and rename Main.java to Mainprint.java

--- pharma_gestionner/src/Mainprint.java
+++ pharma_gestionner/src/Mainprint.java
@@ -1,18 +1,19 @@
 
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Mainprint extends Application {
 
     @Override
-    public void start(Stage primstage) throws Exception {
+    public void start(Stage primstage) throws IOException {
      
         try{
-            Parent root = FXMLLoader.load(getClass().getResource("/sample/sells.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/sample/login.fxml"));
             Scene scene = new Scene(root);
             primstage.setScene(scene);
             primstage.show();
@@ -22,11 +23,11 @@ public class Main extends Application {
         }
     }
 
+
         public static void main(String[] args){
+       
             launch(args);
         }
 
-
     }
-    
 
